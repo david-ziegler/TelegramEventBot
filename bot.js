0eba5b3d6ed1b757d30522eba2dda6387ce5c91a
David Ziegler <david-ziegler@posteo.de>
2019-07-14 13:50:09 +0200

    testing deployment

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ console.log("Bot server started in the " + process.env.NODE_ENV + " mode");
 
 bot.on("message", msg => {
   const name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, "Heyho " + name + "!!!!").then(() => {
+  bot.sendMessage(msg.chat.id, "CreateEventsBot is working!!!").then(() => {
     // reply sent!
   });
 });
