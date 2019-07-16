7e182be04e58ceecabaef0e514e5e6b2a7cc332d
David Ziegler <david-ziegler@posteo.de>
2019-07-16 22:58:17 +0200

    v1.0.0 create events; delete any other messages

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ const Bot = require("node-telegram-bot-api");
 const Keyboard = require("node-telegram-keyboard-wrapper");
 const token = process.env.TOKEN;
 const ACTIONS = {
-  RSVP: "RSVP"
+  RSVP: "RSVP",
+  START_CREATING_EVENT: "START_CREATING_EVENT",
+  DONT_CREATE_EVENT: "DONT_CREATE_EVENT"
 };
 
 let bot;
@@ -15,59 +17,52 @@ if (process.env.NODE_ENV === "production") {
 console.log("Bot server started in the " + process.env.NODE_ENV + " mode");
 
 let events = {};
-let currentlyCreatingEventID = null;
 
-// Show "Add Event"-Button
-bot.onText(/\/start/i, msg => {
-  const replyKeyboard = new Keyboard.ReplyKeyboard();
-  replyKeyboard.addRow("➕ Neues Event erstellen");
-  console.log("open reply keyboard");
-  bot.sendMessage(
-    msg.chat.id,
-    "Ein Bot zum Erstellen von Events wurde hinzugefügt. Gib \\event ein, um ein neues Event zu erstellen oder drücke auf den entsprechenden Button unten.",
-    replyKeyboard.open({ resize_keyboard: true })
-  );
+const rsvpButton = new Keyboard.InlineKeyboard();
+rsvpButton.addRow({ text: "👍  zusagen", callback_data: ACTIONS.RSVP });
+const yesNoButtons = new Keyboard.InlineKeyboard();
+yesNoButtons.addRow({
+  text: "ja",
+  callback_data: ACTIONS.START_CREATING_EVENT
 });
+yesNoButtons.addRow({ text: "nein", callback_data: ACTIONS.DONT_CREATE_EVENT });
 
-// Trigger Create Event
-bot.onText(/(➕ Neues Event erstellen|\/event)/, msg => {
-  if (!currentlyCreatingEventID) {
-    bot.deleteMessage(msg.chat.id, msg.message_id);
-    bot
-      .sendMessage(
-        msg.chat.id,
-        "*Neues Event erstellen*\nGib eine Beschreibung mit Ort & Zeit ein und sende dann die Nachricht ab um das Event zu erstellen.",
-        { parse_mode: "markdown" }
-      )
-      .then(createdMsg => {
-        currentlyCreatingEventID = createEventIDFromMessage(createdMsg);
-      });
-  }
-});
-
-// Create Event
 bot.on("message", msg => {
-  if (currentlyCreatingEventID && msg.text !== "➕ Neues Event erstellen") {
-    bot.deleteMessage(msg.chat.id, msg.message_id);
-    const inlineKeyboard = new Keyboard.InlineKeyboard();
-    inlineKeyboard.addRow({ text: "👍  zusagen", callback_data: ACTIONS.RSVP });
-    bot
-      .editMessageText(msg.text, {
-        chat_id: getChatIDFromEventID(currentlyCreatingEventID),
-        message_id: getMessageIDFromEventID(currentlyCreatingEventID),
-        parse_mode: "markdown",
-        ...inlineKeyboard.build()
-      })
-      .then(createdMsg => {
-        const eventID = currentlyCreatingEventID;
-        events[eventID] = {
-          text: createdMsg.text,
-          attendees: []
-        };
-        currentlyCreatingEventID = null;
-      });
+  if (isEventText(msg.text)) {
+    createEvent(msg);
+  } else {
+    deleteMessage(msg);
   }
 });
+
+function isEventText(text) {
+  return text.indexOf("/event") === 0;
+}
+
+function createEvent(msg) {
+  const eventDescription = removeBotCommand(msg.text);
+  deleteMessage(msg);
+  bot
+    .sendMessage(msg.chat.id, eventDescription, {
+      parse_mode: "markdown",
+      ...rsvpButton.build()
+    })
+    .then(createdMsg => {
+      const eventID = createEventIDFromMessage(createdMsg);
+      events[eventID] = {
+        text: eventDescription,
+        attendees: []
+      };
+    });
+}
+
+function removeBotCommand(text) {
+  return text.replace("/event ", "");
+}
+
+function deleteMessage(msg) {
+  bot.deleteMessage(msg.chat.id, msg.message_id);
+}
 
 // RSVP to Event
 bot.on("callback_query", query => {
@@ -75,37 +70,28 @@ bot.on("callback_query", query => {
   const eventID = createEventIDFromMessage(query.message);
   attendees = events[eventID].attendees.concat(nameOfNewAttendee);
   events[eventID].attendees = attendees;
-  bot
-    .answerCallbackQuery(query.id, { text: "Action received!" })
-    .then(function() {
-      bot.editMessageText(getEventTextWithAttendees(events[eventID]), {
-        chat_id: query.message.chat.id,
-        message_id: query.message.message_id,
-        parse_mode: "markdown"
-      });
+  bot.answerCallbackQuery(query.id, { text: "" }).then(function() {
+    bot.editMessageText(getEventTextWithAttendees(events[eventID]), {
+      chat_id: query.message.chat.id,
+      message_id: query.message.message_id,
+      parse_mode: "markdown",
+      ...rsvpButton.build()
     });
+  });
 });
 
 function getFullNameString(user) {
-  return `${user.first_name} ${user.last_name} (${getCleanMarkdownReadyUsername(
-    user.username
-  )})`;
+  return [user.first_name, user.last_name]
+    .filter(namePart => namePartIsPresent(namePart))
+    .join(" ");
 }
 
-function createEventIDFromMessage(message) {
-  return `${message.chat.id}-${message.message_id}`;
+function namePartIsPresent(namePart) {
+  return !!namePart;
 }
 
-function getChatIDFromEventID(eventID) {
-  return eventID.split("-")[0];
-}
-
-function getMessageIDFromEventID(eventID) {
-  return eventID.split("-")[1];
-}
-
-function getCleanMarkdownReadyUsername(username) {
-  return `@${username.replace(/_/g, "\\_")}`;
+function createEventIDFromMessage(msg) {
+  return `${msg.chat.id}_${msg.message_id}`;
 }
 
 function getEventTextWithAttendees(event) {
