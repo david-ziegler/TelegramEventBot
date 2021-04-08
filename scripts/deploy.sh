39173b5961b7993bd46c880edb7b75d8c0dd068f
David Ziegler <david-ziegler@posteo.de>
2021-04-08 23:25:55 +0200

    new / change path in deployment-script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
-scp index.ts ziedav@delphinus.uberspace.de:~/telegramEventBot/
+scp index.ts ziedav@delphinus.uberspace.de:~/eventBot/
 
-scp -r src ziedav@delphinus.uberspace.de:~/telegramEventBot/
-scp -r scripts ziedav@delphinus.uberspace.de:~/telegramEventBot/
+scp -r src ziedav@delphinus.uberspace.de:~/eventBot/
+scp -r scripts ziedav@delphinus.uberspace.de:~/eventBot/
 
-scp package.json ziedav@delphinus.uberspace.de:~/telegramEventBot/
-scp yarn.lock ziedav@delphinus.uberspace.de:~/telegramEventBot/
+scp package.json ziedav@delphinus.uberspace.de:~/eventBot/
+scp yarn.lock ziedav@delphinus.uberspace.de:~/eventBot/
 
-scp tsconfig.json ziedav@delphinus.uberspace.de:~/telegramEventBot/
-scp .eslintrc.json ziedav@delphinus.uberspace.de:~/telegramEventBot/
-scp .eslintignore ziedav@delphinus.uberspace.de:~/telegramEventBot/
+scp tsconfig.json ziedav@delphinus.uberspace.de:~/eventBot/
+scp .eslintrc.json ziedav@delphinus.uberspace.de:~/eventBot/
+scp .eslintignore ziedav@delphinus.uberspace.de:~/eventBot/
