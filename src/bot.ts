ecda7bb98849d438ac8e110375785b5b5b2d257c
David Ziegler <david-ziegler@posteo.de>
2021-04-24 16:15:45 +0200

    new / catch all errors with global try-catch

--- src/bot.ts
+++ src/bot.ts
@@ -5,6 +5,7 @@ import { DB } from './db';
 import { i18n } from './stuff/i18n';
 import { ENV } from './stuff/environment-variables';
 import { changeRSVPForUser, createEvent } from './usecases';
+import { pretty } from './stuff/pretty';
 
 const db = new DB();
 
@@ -12,10 +13,17 @@ export const bot = new TelegramBot(ENV.BOT_TOKEN, { polling: true });
 console.log(`Bot server started. Version ${packageInfo.version}. Production mode: ${ENV.PRODUCTION_MODE}`);
 
 bot.onText(/^\/(E|e)vent.*/, async (message: Message) => {
-  await createEvent(message, i18n, db, bot);
+  try {
+    await createEvent(message, i18n, db, bot);
+  } catch (error) {
+    console.error(`Error while creating event. Error: ${error}. Command: ${pretty(message)}`);
+  }
 });
 
 bot.on('callback_query', (query: CallbackQuery) => {
-  changeRSVPForUser(query.message, query.id, query.from, query.data, i18n, db, bot);
+  try {
+    changeRSVPForUser(query.message, query.id, query.from, query.data, i18n, db, bot);
+  } catch (error) {
+    console.error(`Error while creating event. Error: ${error}. Command: ${pretty(query)}`);
+  }
 });
-
